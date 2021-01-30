b8527de53ef6a8992b575c6084ecbcbdceb2081e
wrac4242 <42539664+wrac4242@users.noreply.github.com>
2021-01-30 12:23:09 +0000

    basic plan and files

--- source/preprocessing.c
+++ source/preprocessing.c
@@ -3,15 +3,7 @@
 
 //takes in an image, and produces an ascii version of it
 
-int main( int argc, char *argv[] )  {
-
-   if( argc == 2 ) {
-      printf("The file supplied is %s\n", argv[1]);
-   }
-   else if( argc > 2 ) {
-      printf("Too many arguments supplied.\n");
-   }
-   else {
-      printf("One argument expected.\n");
-   }
+int main()  {
+   //converts all .png files inside ../imagesForProcessing into the ascii version
+   //this ascii version is stored inside ../tempFiles
 }
