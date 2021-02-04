287e30183b9750847ab13671f4af69556586e0b0
wrac4242 <42539664+wrac4242@users.noreply.github.com>
2021-02-04 11:07:16 +0000

    added more details and plans

--- source/preprocessing.c
+++ source/preprocessing.c
@@ -10,4 +10,12 @@ int main()  {
    //as source video is black-grey-white, RGB are all the same, and so we can use any for brightness
    //so map the colour 0-255 to a coresponding ascii character, based on brighness
    //output as a text file
+   //each frame will be its own text file
+   //
+   //how tall and wide? has to be some factor of 1980x1080 
+   //170 is the max width
+   //so why not go max width 160, as it is a nice clean factor,
+   //each character is 12x12 pixels
+   //need to get average for them then
+   //
 }
