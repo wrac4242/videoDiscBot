2c1e7326a18c07622335b36801a969ee329f7a9a
wrac4242 <42539664+wrac4242@users.noreply.github.com>
2021-02-04 10:37:12 +0000

    added more comments, planning out more

--- source/preprocessing.c
+++ source/preprocessing.c
@@ -6,4 +6,8 @@
 int main()  {
    //converts all .png files inside ../imagesForProcessing into the ascii version
    //this ascii version is stored inside ../tempFiles
+   
+   //as source video is black-grey-white, RGB are all the same, and so we can use any for brightness
+   //so map the colour 0-255 to a coresponding ascii character, based on brighness
+   //output as a text file
 }
